bf9f151f2df6fad3fb85760bec09b1a0281dd49c
RickyZi <55984119+RickyZi@users.noreply.github.com>
2025-06-09 14:24:26 +0200

    Update focalLoss.py

--- notebook_demo/focalLoss.py
+++ notebook_demo/focalLoss.py
@@ -1,3 +1,3 @@
-# from utilscripts.focalLoss import FocalLoss
-from notebook_demo.demo_utils import FocalLoss
+from utilscripts.focalLoss import FocalLoss
+# from notebook_demo.demo_utils import FocalLoss
 # do to load FT models with the correct definition of FocalLoss
